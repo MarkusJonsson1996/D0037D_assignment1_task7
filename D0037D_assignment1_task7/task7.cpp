b247828b55c865b0e30494f8ecaa96c2ce9d54ab
Markus Jonsson <64263599+MarkusJonsson1996@users.noreply.github.com>
2020-11-11 11:16:43 +0100

    Finished

--- D0037D_assignment1_task7/task7.cpp
+++ D0037D_assignment1_task7/task7.cpp
@@ -6,22 +6,39 @@ using namespace std;
 int main() {
 	string u_in;
 	bool run = true;
+	bool u_in_corr;
 
-	while(run)
-		cout << "Enter a sequence of number: ";
+	while (run) {
+		cout << "Enter a sequence of number(s): ";
 
 		cin >> u_in;
+		u_in_corr = true;
 
 		for (int i = 0; i < u_in.size(); i++) {
-			if (u_in[i] == '-')
-				cout << "Error: Can't be a negative number" << endl;
+			if (u_in[i] > '9' || u_in[i] < '0') {
+				cout << "Error: Sequence can only contain positive number(s)" << endl << endl;;
+				u_in_corr = false;
 				break;
+			}
 		}
 		
-		//u_in.clear();
-	/*for (int i = 0; i < u_in.size(); i++) {
-		cout <<
-	}*/
+		if (u_in_corr) {
+			cout << endl;
+
+			for (int i = 0; i < u_in.size(); i++) {
+				cout << u_in[i] << endl;
+			}
+
+			cout << "---------" << endl;
+
+			for (int i = u_in.size()-1; i >= 0; i--) {
+				cout << u_in[i] << endl;
+			}
+
+			run = false;
+		}
+
+	}
 	return 0;
 
 }
